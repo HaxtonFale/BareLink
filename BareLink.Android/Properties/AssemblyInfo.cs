dedbc8bfbe9bdfbdb02325f708e468e122103f3c
Haxton Fale <haxtonfale@gmail.com>
2021-12-30 17:48:16 +0000

    Remove extra attributes

--- BareLink.Android/Properties/AssemblyInfo.cs
+++ BareLink.Android/Properties/AssemblyInfo.cs
@@ -21,8 +21,6 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
